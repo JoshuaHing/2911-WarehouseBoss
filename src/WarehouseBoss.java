bdb1b79b6f6c86ee24cdc3fa5610b1eb0b424d25
Qi Chai <q.chai@student.unsw.edu.au>
2017-05-25 10:26:03 +1000

    Delete useless import

--- src/WarehouseBoss.java
+++ src/WarehouseBoss.java
@@ -1,8 +1,4 @@
-import java.awt.EventQueue;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 // COMP2911 project-2017S1
 
